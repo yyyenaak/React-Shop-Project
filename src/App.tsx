349eb95f85fb41bb44e995bbc3dbde0b0cb883a6
yyyenaak <dpsk185@gmail.com>
2025-02-28 17:03:30 +0900

    이미지, 컨테이너 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./App.css";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">Shoppp</Navbar.Brand>
           <Nav className="me-auto">
@@ -16,6 +17,32 @@ function App() {
       </Navbar>
 
       <div className="main-bg"></div>
+
+      <Container>
+        <Row>
+          <Col>
+            <img src={process.env.PUBLIC_URL + "/logo192.png"} width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
