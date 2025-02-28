87475cee64e2e257f95f5a8bb0a6f8886a90ed08
yyyenaak <dpsk185@gmail.com>
2025-02-28 17:31:49 +0900

    component 생성 및 data.js 활용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import data from "./data.js";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div>
       <Navbar bg="light" variant="light">
@@ -30,24 +33,24 @@ function App() {
               src="https://codingapple1.github.io/shop/shoes1.jpg"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </Col>
           <Col>
             <img
               src="https://codingapple1.github.io/shop/shoes2.jpg"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </Col>
           <Col>
             <img
               src="https://codingapple1.github.io/shop/shoes3.jpg"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
           </Col>
         </Row>
       </Container>
