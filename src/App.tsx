c13b840f4543a24d35a830eb29bd1bb39f97ee42
yyyenaak <dpsk185@gmail.com>
2025-02-28 16:51:07 +0900

    background 이미지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,21 @@
 import React from "react";
 import "./App.css";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar expand="lg" className="bg-body-tertiary">
+    <div>
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">HaloShop</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar.Brand href="#home">Shoppp</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg"></div>
     </div>
   );
 }
